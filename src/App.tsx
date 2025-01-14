01d42f13b24aa5fa858e51a91e688b4667668a3d
logan-tm <logan@logantm.com>
2025-01-14 14:47:16 -0700

    Removed bloat from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+
+import outputs from "../amplify_outputs.json";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(outputs);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          This is the <code>DEV</code> environment
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <div className="card">
+            <p>
+              This is the <code>{import.meta.env.VITE_ENV_BRANCH}</code>{" "}
+              environment
+            </p>
+            <p>Welcome, {user?.username}</p>
+            <button onClick={signOut}>Sign Out</button>
+          </div>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
