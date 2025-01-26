3ec942511109b15f42991bebd02e010b76135f30
logan-tm <logan@logantm.com>
2025-01-25 20:21:48 -0700

    Added 'check branch' workflow

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import "@aws-amplify/ui-react/styles.css";
 import outputs from "../amplify_outputs.json";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import useAuth from "./hooks/useAuth";
 
 Amplify.configure(outputs);
 
 function App() {
+  // const { username, userId, signInDetails } = await getCurrentUser();
+  const { user: hookUser, isLoading } = useAuth();
+  console.log("USER", hookUser);
   return (
-    <Authenticator>
+    <Authenticator signUpAttributes={[]}>
       {({ signOut, user }) => (
         <main>
           <div className="card">
@@ -17,7 +21,8 @@ function App() {
               This is the <code>{import.meta.env.VITE_ENV_BRANCH}</code>{" "}
               environment
             </p>
-            <p>Welcome, {user?.username}</p>
+            {isLoading && <p>Loading...</p>}
+            {user && <p>Welcome, {user.signInDetails?.loginId}</p>}
             <button onClick={signOut}>Sign Out</button>
           </div>
         </main>
